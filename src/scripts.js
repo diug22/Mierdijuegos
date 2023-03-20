89155397cc9361433372f33b537e172fd83e8641
diug22 <brotons22@gmail.com>
2023-03-20 15:50:42 +0100

    gif change

--- src/scripts.js
+++ src/scripts.js
@@ -1,44 +1,48 @@
-function generateCarouselItems(fileList) {
-  const carouselInner = document.querySelector(".carousel-inner");
+function generateCardItems(fileList) {
+  const cardContainer = document.getElementById("card-container");
 
-  fileList.forEach((file, index) => {
-    const carouselItem = document.createElement("div");
-    carouselItem.classList.add("carousel-item");
-    if (index === 0) {
-      carouselItem.classList.add("active");
-    }
+  fileList.forEach((file) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-    const link = document.createElement("a");
-    link.href = file.path;
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
 
     const img = document.createElement("img");
     img.src = file.imageUrl;
     img.alt = file.name;
-    img.style.width = "100%";
-    img.style.height = "auto";
+    img.classList.add("card-img-top");
 
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = file.name;
+
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.textContent = file.rating;
+
+    const link = document.createElement("a");
+    link.href = file.path;
     link.appendChild(img);
-    carouselItem.appendChild(link);
 
-    const carouselCaption = document.createElement("div");
-    carouselCaption.classList.add("carousel-caption", "d-none", "d-md-block");
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
 
-    const captionLink = document.createElement("a");
-    captionLink.href = file.path;
-    captionLink.textContent = file.name;
-    captionLink.style.color = "white";
+    card.appendChild(link);
+    card.appendChild(cardBody);
 
-    carouselCaption.appendChild(captionLink);
-    carouselItem.appendChild(carouselCaption);
+    const col = document.createElement("div");
+    col.classList.add("col-md-4", "col-sm-6");
+    col.appendChild(card);
 
-    carouselInner.appendChild(carouselItem);
+    cardContainer.appendChild(col);
   });
 }
 
 function init() {
   fetch("src/fileList.json")
     .then((response) => response.json())
-    .then((data) => generateCarouselItems(data))
+    .then((data) => generateCardItems(data))
     .catch((error) => console.error("Error:", error));
 }
 
